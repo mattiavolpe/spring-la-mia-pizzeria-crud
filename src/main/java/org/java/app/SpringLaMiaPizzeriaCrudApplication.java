55e0ce857f3d3053280486d19d97e6c79f125bab
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-10-06 17:48:34 +0200

    ADD aesthetic elements

--- src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/java/app/SpringLaMiaPizzeriaCrudApplication.java
@@ -21,12 +21,15 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Pizza pizza1 = new Pizza("Margherita", "Best pizza in the world", "https://images.pexels.com/photos/6605214/pexels-photo-6605214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 700);
+		Pizza pizza1 = new Pizza("Margherita", "Tomato sauce and mozzarella", "https://images.pexels.com/photos/6605214/pexels-photo-6605214.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 700);
 		
-		Pizza pizza2 = new Pizza("Tonno e cipolla", "Second best pizza in the world", "https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 900);
+		Pizza pizza2 = new Pizza("Tonno e cipolla", "Natural tuna, red onions and mozzarella", "https://www.galbani.it/sites/default/files/styles/width_1920/public/se_tonno_buona_da_star_bene_980x357.jpg", 900);
+		
+		Pizza pizza3 = new Pizza("Capricciosa", "Tomato sauce, artichokes, mushrooms, anchovies, black olives and mozzarella", "https://images.pexels.com/photos/1049626/pexels-photo-1049626.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 900);
 		
 		pizzaService.savePizza(pizza1);
 		pizzaService.savePizza(pizza2);
+		pizzaService.savePizza(pizza3);
 		
 		List<Pizza> pizzas = pizzaService.findAll();
 		
